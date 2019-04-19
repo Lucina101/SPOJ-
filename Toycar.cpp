76d73750605508ee585895114f14da928483667a
Manza21 <supakornmanza21@gmail.com>
2019-04-19 21:15:17 +0700

    Update Toycar.cpp

--- Toycar.cpp
+++ Toycar.cpp
@@ -4,7 +4,22 @@ const int maxn=5e5+10,inf=1e9;
 int t,n,k,p,x,v[maxn],ans,c;
 vector<int> a[maxn];
 bool b[maxn];
-
+/*
+3 cases
+1) Toy is at the floor-> Do nothing
+2) Toy is on the shelf and floor has space left-> Just put toys in
+3) Floor is full and Toy is on the shelf-> Swap it with toy that has greatest next play
+Why does this work? 1 and 2 are trivial Let's look at 3)
+Assume that you swap toys with smaller next play. The answer can't be better right.
+You still have to swap Toy with greatest next play and swap some toys before it.
+If you swap toy with the greatest next play, it's better that toys with less next play are still on the floor. And no need to 
+swap in and out to play.
+*/
+/*
+Another comment
+This problem can be also solve by queue or list. List also support operation push and pop_front like queue.
+You don't have to reverse vector if you use list. :)
+*/
 int main(){
     scanf("%d",&t);
     while(t--){
